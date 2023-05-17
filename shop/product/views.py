d0bce6ae5598a3cbf9af09f10f47bd2d351e3b52
Tushar Sahani <tusharsahani000@gmail.com>
2023-05-17 23:53:05 +0530

    feedback review

--- shop/product/views.py
+++ shop/product/views.py
@@ -46,9 +46,11 @@ def product_page(request,product_brand,product_slug):
             )
             feeback.save()
             messages.success(request,"Your form is Subimited")
+        allFeedbacks = Feedback.objects.filter(product = product)
         return render(request, "product/products.html",{
         "product": product,
         "forms":form,
+        "allFeedbacks":allFeedbacks,
     })
 
     
