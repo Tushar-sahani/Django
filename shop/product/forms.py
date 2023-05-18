ba49b634ba3232c8e1fca4ccda7af10f1791f019
Tushar Sahani <tusharsahani000@gmail.com>
2023-05-18 23:51:08 +0530

    Model forms

--- shop/product/forms.py
+++ shop/product/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-
+from .models import Feedback
+'''
 class Feedbackform(forms.Form):
     name = forms.CharField(required=True,)
     rating = forms.IntegerField(min_value=1, max_value=5 )
     text = forms.CharField(label="Your Feedback",widget=forms.Textarea,max_length=200)
-    
+'''
+class Feedbackform(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ["name","rating","text"]
+        label = {"name":"Full Name"}
